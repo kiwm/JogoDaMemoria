e7b3a5b52bb820406132229328877efca29930f8
kiwm <pedromaltapedrozo@gmail.com>
2019-09-22 22:48:48 -0300

    logica de funcionamento adicionada

--- modelo.js
+++ modelo.js
@@ -1,11 +1,12 @@
 var imagens = ["gato.png", "macaco.png", "porco.png", "ovelha.png", "rinoceronte.png", "gato.png", "macaco.png", "porco.png", "ovelha.png", "rinoceronte.png", "rato.png", "rato.png"]
+var primeiro = "-1"
+var segundo = "-1"
 function prepararJogo() {
     shuffleArray(imagens)
     for(var i = 0; i < imagens.length; ++i) {
         var img = document.getElementById(`img${i}`)
         img.src = imagens[i]
     }
-
     iniciarJogo()
 }
 
@@ -27,5 +28,37 @@ function iniciarJogo() {
 
 function clicar(x) {
     var img = document.getElementById(`img${x}`)
+  
     img.src = imagens[x]
+    comparar(x)
+}
+
+function comparar(x) {
+    if(primeiro == "-1") {
+        primeiro = x
+
+    } else if(segundo == "-1") {
+        segundo = x
+
+    }
+
+    if(primeiro != "-1" && segundo != "-1") {
+        
+        var img1 = document.getElementById(`img${primeiro}`)
+        var img2 = document.getElementById(`img${segundo}`)
+
+        if(img1.src == img2.src) {
+            img1.onclick =""   
+            img2.onclick =""  
+            img1.src = "branco.png"
+            img2.src = "branco.png"
+            primeiro = "-1"
+            segundo = "-1"
+        } else {
+            img1.src = "fundo.png"
+            img2.src = "fundo.png"
+            primeiro = "-1"
+            segundo = "-1"
+        }     
+    }
 }
